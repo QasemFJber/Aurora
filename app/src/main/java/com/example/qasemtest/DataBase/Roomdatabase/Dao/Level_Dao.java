5f2ee2ac08f689ff027edeecf944a9a393d03ea4
Qasem <jaberqasem75@gmail.com>
2022-12-27 20:25:02 +0200

    Aurora Game

--- app/src/main/java/com/example/qasemtest/DataBase/Roomdatabase/Dao/Level_Dao.java
+++ app/src/main/java/com/example/qasemtest/DataBase/Roomdatabase/Dao/Level_Dao.java
@@ -3,5 +3,5 @@ package com.example.qasemtest.DataBase.Roomdatabase.Dao;
 import androidx.room.Dao;
 
 @Dao
-public interface Puzzles_Dao {
+public interface Level_Dao {
 }
--- app/src/main/java/com/example/qasemtest/DataBase/Roomdatabase/Dao/Question_Dao.java
+++ app/src/main/java/com/example/qasemtest/DataBase/Roomdatabase/Dao/Question_Dao.java
@@ -3,5 +3,7 @@ package com.example.qasemtest.DataBase.Roomdatabase.Dao;
 import androidx.room.Dao;
 
 @Dao
-public interface Stages_Dao {
+public interface Question_Dao {
+
+
 }
